22b1516de01a8a65859eac2260dd89bcfd1e9f13
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-01-15 17:59:46 +0100

    adding comments to rationalize implementation

--- src/logger.h
+++ src/logger.h
@@ -5,7 +5,13 @@
 #include <glog/logging.h>
 
 #include <iostream>
-// todo: why use this static function here?
+
+/**
+ * Linkage of logger function is internal, i.e., only visible to
+ * the logger.h translation unit. Making the function static ou-
+ * tside a class or struct is similar to making the scope of a
+ * function private in a class.
+ */
 static void logger(int& t_argc, char** t_argvptr)
 {
     google::ParseCommandLineFlags(&t_argc, &t_argvptr, true);
